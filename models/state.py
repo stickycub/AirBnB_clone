73d9f84f23c197b6d13082ae76193d368fe03ea7
Benjamin <krazy2cub@gmail.com>
2024-02-09 04:29:19 -0800

    models

--- models/state.py
+++ models/state.py
@@ -4,8 +4,7 @@
 from models.base_model import BaseModel
 
 
-class City(BaseModel):
-    """Class for managing city objects"""
+class State(BaseModel):
+    """Class for managing state objects"""
 
-    state_id = ""
     name = ""
